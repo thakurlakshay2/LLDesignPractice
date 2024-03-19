35192f83b2efb10056f47f1c3a7ed94f863ee524
thakurlakshay2 <thakurlakshay2@gmail.com>
2024-03-19 23:14:23 +0530

    movie ticket LLD - part 2 -small change start creating API

---
+++ src/MovieTicketBooking/api/MovieController.java
@@ -0,0 +1,4 @@
+package MovieTicketBooking.api;
+
+public class MovieController {
+}
---
+++ src/MovieTicketBooking/api/TheatreController.java
@@ -0,0 +1,22 @@
+package MovieTicketBooking.api;
+
+import MovieTicketBooking.model.Screen;
+
+public class TheatreController {
+    final private TheatreService theatreService;
+
+    public String createTheatre(final String theatreName){
+        return theatreService.createTheatre(theatreName).getId();
+
+    }
+
+    public String createScreenInTheatre(final String screenName,final String theatreId){
+        final Theatre theatre=theatreService.getTheatre(theatreId);
+        return theatreService.createScreenInTheatre(screenName,theatre).getId();
+    }
+
+    public final createSeatInScreen(final Integer rowNo, final Integer seatNo, final String screenId){
+        final Screen screen=theatreService.getTheatre(screenId);
+        return theatreService.createSeatInScreen(rowNo,seatNo,screen);
+    }
+}
